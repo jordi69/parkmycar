e5ac5d3afc20496c3a19162e2ff37cbfdfc8de07
jordi69 <antwerp_boyke1@hotmail.com>
2015-10-13 14:33:36 +0200

    authentication en mockups toegevoegd

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Welcome</title>
+</head>
+<body>
+	<div class="container">
+		<div class="content">
+			<a href="/auth/login">Login</a>
+    		<a href="/auth/register">Register</a>
+    		<a href="/">Home</a>
+			@yield('content')
+		</div>
+	</div>
+</body>
+</html>
