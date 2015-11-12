6bd19ff14ed3b94781fa5ecd32b011e239b34f79
laurenswuyts <Laurenswuyts14@gmail.com>
2015-11-12 13:07:49 +0100

    azer
    eraera

--- config/database.php
+++ config/database.php
@@ -55,7 +55,7 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => 'localhost',
-            'database'  => 'phpmyadmin',
+            'database'  => 'parkmycar',
             'username'  => 'root',
             'password'  => '',
             'charset'   => 'utf8',
