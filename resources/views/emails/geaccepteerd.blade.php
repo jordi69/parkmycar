1adf8c69c83a4c17cf6c7e7cd2a879702cce7090
laurenswuyts <Laurenswuyts14@gmail.com>
2015-11-15 18:31:36 +0100

    mlkmk
    mlklmk

---
+++ resources/views/emails/geaccepteerd.blade.php
@@ -0,0 +1,7 @@
+<p> Beste, </p>
+
+<p> Jou aanvraag voor de parkeerplaats is geaccepteerd.!</p>
+
+<p> Ga het snel checken op je profiel! http://parkmycar.herokuapp.com/profile</p>
+
+<p> Met vriendelijke groeten,</p>
