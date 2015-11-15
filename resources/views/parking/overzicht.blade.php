c5313fbf08b363a3230b3a79f009284e4942c2ac
laurenswuyts <Laurenswuyts14@gmail.com>
2015-11-15 12:59:27 +0100

    ezar
    raerz

--- resources/views/parking/overzicht.blade.php
+++ resources/views/parking/overzicht.blade.php
@@ -1,4 +1,36 @@
 @extends('layouts.master')
 @section('content')
-<p>hier moet de lijst met alle parkeerplaatsen komen</p>
+<div class="listparking">
+ 	@foreach ($items as $item)
+ 	<script>
+ 	function initialize() {
+    	var mapCanvas = document.getElementById('map');
+    	var myLatlng = new google.maps.LatLng({{$item->latitude}}, {{$item->longitude}});
+        var mapOptions = {
+          center: myLatlng,
+          zoom: 20,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        var marker = new google.maps.Marker({
+    		position: myLatlng,
+    		map: map,
+  		});
+        var map = new google.maps.Map(mapCanvas, mapOptions)
+        marker.setMap(map);
+	}
+    google.maps.event.addDomListener(window, 'load', initialize);
+ 	</script>
+ 	<div class="listpark col-sm-4">
+ 	<br>
+    	<div id="map" style="width:100%;height:280px;"></div>
+    	<div class="info col-sm-12">
+    	<br>
+    	<p style="text-align:center;">Van <span style="font-weight: bold;">{{date('H:i', strtotime($item->BeschikbaarStarttijd))}}</span> tot <span style="font-weight: bold;">{{date('H:i', strtotime($item->BeschikbaarStoptijd))}}.</span></p>
+    	<form style="width:100%;padding-top:5px;margin-top:0;" method="POST" action="/parkeren">
+    	    {!! csrf_field() !!}
+    		<input type="submit" value="PARKEREN">
+    	</form>
+    	</div>
+    </div>
+	@endforeach
 @endsection
