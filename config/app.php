18a87b0f7de59610cf8ac488baf805db52f59bec
laurenswuyts <Laurenswuyts14@gmail.com>
2015-11-15 22:13:13 +0100

    ezar
    aeraz

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', yes),
 
     /*
     |--------------------------------------------------------------------------
@@ -78,7 +78,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY', 'SomeRandomString'),
+    'key' => 'GTssrhksQsBvAG7JWyvCy2K00jhTE1Vb',
 
     'cipher' => 'AES-256-CBC',
 
